116d0b912d28954e61885aed8bea5120e6b6ccd9
Nathan Towe <nathanwtowe89@gmail.com>
2023-10-23 19:20:59 -0400

    sending index and show data{

---
+++ sever.js
@@ -0,0 +1,24 @@
+//dependencies
+const express = require('express')
+
+//configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+console.log(PORT)
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+
+//Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+
+//listen
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+})
