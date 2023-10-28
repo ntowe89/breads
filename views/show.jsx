4725833bb63b7abd8ee122721664a687e0cc17c8
Nathan Towe <nathanwtowe89@gmail.com>
2023-10-28 16:27:25 -0400

    sending index and show data

--- views/show.jsx
+++ views/show.jsx
@@ -1,23 +1,23 @@
 const React = require('react')
-const Default = require('./layouts/Default')
+const Default = require('./layouts/default')
 
 function Show ({bread}) {
     // Confirm we are getting our bread data in the terminal.
     // console.log(bread.name)
       return (
         <Default>
-          <h2>Show Page</h2>
           <h3>{bread.name}</h3>
           <p>
-            and it 
+            and it   
             {
                 bread.hasGluten
-                ? <span>does</span>
-                : <span>does NOT</span>
+                ? <span> does </span>
+                : <span> does NOT </span>
             }
             have gluten.
           </p>
           <img src={bread.image} alt={bread.name} />
+          <li><a href="/breads">Go home</a></li>
         </Default>
       )
   }
