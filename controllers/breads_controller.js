1adf2fa207929e27638d996386ce4959ef08804f
Nathan Towe <nathanwtowe89@gmail.com>
2023-11-04 10:53:08 -0400

    have everything working, added edit and update

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -29,8 +29,15 @@ breads.get('/:id', (req, res) => {
       })
 })
 
-
-
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
+})
 
 // CREATE
 breads.post('/', (req, res) => {
@@ -46,13 +53,29 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+//UPDATE
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
+})
 
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-    Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
-  })
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
+})
+
 
 
   module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -19,7 +19,10 @@ function Show ({bread, index}) {
           <img src={bread.image} alt={bread.name} />
           <li><a href="/breads">Go home</a></li>
 
-          <form action={`/breads/${index}?_method=DELETE`} method="POST">
+          <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+
+
+          <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
             <input type='submit' value="DELETE"/>
           </form>
 
